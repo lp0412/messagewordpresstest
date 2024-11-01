b46206db758d8712427b9e920b63cb4c822f1c99
lp0412 <161710952+lp0412@users.noreply.github.com>
2024-11-01 01:50:56 +0100

    Delete views directory

--- views/layout-index.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined( 'ABSPATH' ) || exit;
-$initialHeight = (int) apply_filters( 'bp_better_messages_max_height', Better_Messages()->settings['messagesHeight'] );
-echo '<div class="bp-messages-wrap-main" style="height: ' . $initialHeight . 'px">' . Better_Messages()->functions->container_placeholder() . '</div>';
